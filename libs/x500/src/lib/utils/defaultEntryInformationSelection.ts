2959ea3f3ecca905265c821bfb7c6ec7bc4922c7
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-07-03 01:12:13 -0400

    Create defaultEntryInformationSelection
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

---
+++ libs/x500/src/lib/utils/defaultEntryInformationSelection.ts
@@ -0,0 +1,52 @@
+import { EntryInformationSelection } from "../modules/DirectoryAbstractService/EntryInformationSelection.ta";
+
+// EntryInformationSelection ::= SET {
+//     attributes                     CHOICE {
+//       allUserAttributes         [0]  NULL,
+//       select                    [1]  SET OF AttributeType
+//       -- empty set implies no attributes are requested -- } DEFAULT allUserAttributes:NULL,
+//       infoTypes               [2]  INTEGER {
+//         attributeTypesOnly        (0),
+//         attributeTypesAndValues   (1)} DEFAULT attributeTypesAndValues,
+//     extraAttributes                CHOICE {
+//       allOperationalAttributes  [3]  NULL,
+//       select                    [4]  SET SIZE (1..MAX) OF AttributeType } OPTIONAL,
+//     contextSelection               ContextSelection OPTIONAL,
+//     returnContexts                 BOOLEAN DEFAULT FALSE,
+//     familyReturn                   FamilyReturn DEFAULT
+//                                      {memberSelect contributingEntriesOnly} }
+
+/**
+ * @summary Convenience default value for EntryInformationSelection.
+ *
+ * ### ASN.1 Definition
+ *
+ * ```asn1
+ * EntryInformationSelection ::= SET {
+ *     attributes                     CHOICE {
+ *       allUserAttributes         [0]  NULL,
+ *       select                    [1]  SET OF AttributeType
+ *       -- empty set implies no attributes are requested -- } DEFAULT allUserAttributes:NULL,
+ *       infoTypes               [2]  INTEGER {
+ *         attributeTypesOnly        (0),
+ *         attributeTypesAndValues   (1)} DEFAULT attributeTypesAndValues,
+ *     extraAttributes                CHOICE {
+ *       allOperationalAttributes  [3]  NULL,
+ *       select                    [4]  SET SIZE (1..MAX) OF AttributeType } OPTIONAL,
+ *     contextSelection               ContextSelection OPTIONAL,
+ *     returnContexts                 BOOLEAN DEFAULT FALSE,
+ *     familyReturn                   FamilyReturn DEFAULT
+ *                                      {memberSelect contributingEntriesOnly} }
+ * ```
+ */
+export
+const defaultEntryInformationSelection: EntryInformationSelection = new EntryInformationSelection(
+    EntryInformationSelection._default_value_for_attributes,
+    EntryInformationSelection._default_value_for_infoTypes,
+    undefined,
+    undefined,
+    undefined,
+    EntryInformationSelection._default_value_for_familyReturn,
+);
+
+export default defaultEntryInformationSelection;
