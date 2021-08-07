6a8061b4f84d72b35aa4ae4b5b26a4d214f887fc
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-08-07 18:24:55 -0400

    Add Chained type
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

---
+++ libs/x500/src/lib/types/Chained.ts
@@ -0,0 +1,9 @@
+import { OPTIONALLY_PROTECTED } from "../modules/EnhancedSecurity/OPTIONALLY-PROTECTED.ta";
+import {
+    Chained_ArgumentType_OPTIONALLY_PROTECTED_Parameter1,
+} from "../modules/DistributedOperations/Chained-ArgumentType-OPTIONALLY-PROTECTED-Parameter1.ta";
+
+export
+type Chained = OPTIONALLY_PROTECTED<Chained_ArgumentType_OPTIONALLY_PROTECTED_Parameter1>;
+
+export default Chained;
