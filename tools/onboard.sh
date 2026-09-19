ef63bd5bf97b8081527506beb330f00f68a01232
Jonathan M. Wilbur <jonathan@wilbur.space>
2026-09-19 17:02:52 -0400

    modify onboard.sh to organize imports

--- tools/onboard.sh
+++ tools/onboard.sh
@@ -7,7 +7,7 @@
 set -euo pipefail
 
 packages=(
-    # "acp133"
+    "acp133"
     "ain"
     "c1222"
     "cdt"
@@ -38,41 +38,47 @@ packages=(
     "z3950"
 )
 
-for pkg in "${packages[@]}"; do
-  echo "Onboarding ${pkg}..."
-  git checkout -b "onboard_${pkg}"
-  npx nx g @nx/js:lib packages/${pkg} \
-    --name @wildboar/${pkg} \
-    --bundler=tsc \
-    --linter=none \
-    --unitTestRunner=vitest \
-    --testEnvironment=node \
-    --buildable=true \
-    --compiler=tsc \
-    --useProjectJson=true
-  rm -rf packages/${pkg}/src/*
-  echo 'export {};' > packages/${pkg}/src/index.mts
-  mkdir -p packages/${pkg}/src/lib
-  mkdir -p packages/${pkg}/doc
-  cp ~/Downloads/asn1-compiled-to-ts/${pkg}/asn1/* packages/${pkg}/doc
-  cp -r ~/Downloads/asn1-compiled-to-ts/${pkg}/source/modules packages/${pkg}/src/lib
-  sed "s/spkac/${pkg}/" packages/spkac/project.json > packages/${pkg}/project.json
-  sed "s/spkac/${pkg}/" packages/spkac/package.json > packages/${pkg}/package.json
-  sed "s/spkac/${pkg}/" packages/spkac/jsr.json > packages/${pkg}/jsr.json
-  sed -i 's/"version": "2.0.1"/"version": "0.1.0"/' packages/${pkg}/package.json
-  sed -i 's/"version": "2.0.1"/"version": "0.1.0"/' packages/${pkg}/jsr.json
-  sed "s/PKG_NAME/${pkg}/" packages/tai/README.md > packages/${pkg}/README.md
-  cp packages/v59/LICENSE.txt packages/${pkg}/LICENSE.txt
+# for pkg in "${packages[@]}"; do
+#   echo "Onboarding ${pkg}..."
+#   git checkout -b "onboard_${pkg}"
+#   npx nx g @nx/js:lib packages/${pkg} \
+#     --name @wildboar/${pkg} \
+#     --bundler=tsc \
+#     --linter=none \
+#     --unitTestRunner=vitest \
+#     --testEnvironment=node \
+#     --buildable=true \
+#     --compiler=tsc \
+#     --useProjectJson=true
+#   rm -rf packages/${pkg}/src/*
+#   echo 'export {};' > packages/${pkg}/src/index.mts
+#   mkdir -p packages/${pkg}/src/lib
+#   mkdir -p packages/${pkg}/doc
+#   cp ~/Downloads/asn1-compiled-to-ts/${pkg}/asn1/* packages/${pkg}/doc
+#   cp -r ~/Downloads/asn1-compiled-to-ts/${pkg}/source/modules packages/${pkg}/src/lib
+#   sed "s/spkac/${pkg}/" packages/spkac/project.json > packages/${pkg}/project.json
+#   sed "s/spkac/${pkg}/" packages/spkac/package.json > packages/${pkg}/package.json
+#   sed "s/spkac/${pkg}/" packages/spkac/jsr.json > packages/${pkg}/jsr.json
+#   sed -i 's/"version": "2.0.1"/"version": "0.1.0"/' packages/${pkg}/package.json
+#   sed -i 's/"version": "2.0.1"/"version": "0.1.0"/' packages/${pkg}/jsr.json
+#   sed "s/PKG_NAME/${pkg}/" packages/tai/README.md > packages/${pkg}/README.md
+#   cp packages/v59/LICENSE.txt packages/${pkg}/LICENSE.txt
   
-  git add packages/${pkg}
-  git add package.json
-  git add package-lock.json
-  git add tsconfig.json
-  git commit -m "${pkg}: initial"
-  git push -u origin "onboard_${pkg}"
-  git checkout master
-done
+#   git add packages/${pkg}
+#   git add package.json
+#   git add package-lock.json
+#   git add tsconfig.json
+#   git commit -m "${pkg}: initial"
+#   git push -u origin "onboard_${pkg}"
+#   git checkout master
+# done
 
 for pkg in "${packages[@]}"; do
-  gh pr create --repo Wildboar-Software/asn1-typescript-libraries --title "Onboard ${pkg}" --head onboard_${pkg} --body "Onboard the \`@wildboar/${pkg}\` module." --label onboard --draft --assignee @me
+  git checkout "onboard_${pkg}"
+  npx organize-imports-cli packages/${pkg}/src/lib/modules
+  git add packages/${pkg}/src/lib/modules
+  git commit -m "${pkg}: remove unused imports"
+  git push
+  git checkout master
+  # gh pr create --repo Wildboar-Software/asn1-typescript-libraries --title "Onboard ${pkg}" --head onboard_${pkg} --body "Onboard the \`@wildboar/${pkg}\` module." --label onboard --draft --assignee @me
 done
